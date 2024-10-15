4a8a1506ae6e180f2b18dd5e2a7133bf1d4a7a19
Choi YeJi <yeji9909o@gmail.com>
2024-10-15 14:46:09 +0900

    popup-paging-api init

---
+++ src/main/java/com/example/api/PopupPagingApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopupPagingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PopupPagingApiApplication.class, args);
+	}
+
+}
